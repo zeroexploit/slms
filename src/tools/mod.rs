eaf6891bc4c9d7353d4ff3be53aebbda8802eee1
zeroexploit <zeroexploit@users.noreply.github.com>
2017-09-09 11:24:40 +0200

    Added first simple Logger to feature/logging

--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -4,3 +4,7 @@ pub use self::namevaluepair::NameValuePair;
 mod xmlparser;
 pub use self::xmlparser::XMLParser;
 pub use self::xmlparser::XMLEntry;
+
+mod logger;
+pub use self::logger::Logger;
+pub use self::logger::LogLevel;
