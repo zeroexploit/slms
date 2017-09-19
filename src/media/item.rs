ced31fec55f7049d6adfdc8cba87bad7f87ec1df
zeroexploit <zeroexploit@users.noreply.github.com>
2017-09-19 13:13:59 +0200

    Stabilized Media Item Structures

--- src/media/item.rs
+++ src/media/item.rs
@@ -2,8 +2,7 @@ use super::container::Container;
 use super::stream::Stream;
 use super::thumbnail::Thumbnail;
 use tools::NameValuePair;
-use configuration::RendererConfiguration;
-use configuration::ServerConfiguration;
+use configuration::{RendererConfiguration, ServerConfiguration};
 
 /// # MediaType
 ///
@@ -33,6 +32,10 @@ impl MediaType {
     /// Convert a printable String back to the enumeration.
     /// Used to load the Information from the
     /// Media Database.
+    ///
+    /// # Arguments
+    ///
+    /// * `content` - String representation of the Enum Value
     pub fn from_string(content: &str) -> MediaType {
         match content {
             "1" => MediaType::AUDIO,
@@ -44,6 +47,7 @@ impl MediaType {
 }
 
 /// # MetaData
+///
 /// This structure holds all Meta Information that
 /// are supported by UPnP and can be extracted from
 /// a Media File.
@@ -190,8 +194,119 @@ impl MetaData {
         pair_vec
     }
 
+    /// Will look into the Meta Data and generate an UPnp Compailant
+    /// XML String with all Data that has a Value.
     pub fn generate_upnp_xml(&self) -> String {
-        let xml: String = String::new();
+        let mut xml: String = String::new();
+
+        if self.genre.len() > 0 {
+            xml.push_str(&format!(
+                "&lt;upnp:genre&gt;{}&lt;/upnp:genre&gt;",
+                self.genre
+            ));
+        }
+
+        if self.description.len() > 0 {
+            xml.push_str(&format!(
+                "&lt;dc:description&gt;{}&lt;/dc:description&gt;",
+                self.description
+            ));
+        }
+
+        if self.long_description.len() > 0 {
+            xml.push_str(&format!(
+                "&lt;upnp:longDescription&gt;{}&lt;/upnp:longDescription&gt;",
+                self.long_description
+            ));
+        }
+
+        if self.producer.len() > 0 {
+            xml.push_str(&format!(
+                "&lt;upnp:producer&gt;{}&lt;/upnp:producer&gt;",
+                self.producer
+            ));
+        }
+
+        if self.rating.len() > 0 {
+            xml.push_str(&format!(
+                "&lt;upnp:rating&gt;{}&lt;/upnp:rating&gt;",
+                self.rating
+            ));
+        }
+
+        if self.actor.len() > 0 {
+            xml.push_str(&format!(
+                "&lt;upnp:actor&gt;{}&lt;/upnp:actor&gt;",
+                self.actor
+            ));
+        }
+
+        if self.director.len() > 0 {
+            xml.push_str(&format!(
+                "&lt;upnp:director&gt;{}&lt;/upnp:director&gt;",
+                self.director
+            ));
+        }
+
+        if self.publisher.len() > 0 {
+            xml.push_str(&format!(
+                "&lt;dc:publisher&gt;{}&lt;/dc:publisher&gt;",
+                self.publisher
+            ));
+        }
+
+        if self.album.len() > 0 {
+            xml.push_str(&format!(
+                "&lt;upnp:album&gt;{}&lt;/upnp:album&gt;",
+                self.album
+            ));
+        }
+
+        if self.track_number.len() > 0 {
+            xml.push_str(&format!(
+                "&lt;upnp:originalTrackNumber&gt;{}&lt;/upnp:originalTrackNumber&gt;",
+                self.track_number
+            ));
+        }
+
+        if self.playlist.len() > 0 {
+            xml.push_str(&format!(
+                "&lt;upnp:playlist&gt;{}&lt;/upnp:playlist&gt;",
+                self.playlist
+            ));
+        }
+
+        if self.contributor.len() > 0 {
+            xml.push_str(&format!(
+                "&lt;dc:contributor&gt;{}&lt;/upnp:contributor&gt;",
+                self.contributor
+            ));
+        }
+
+        if self.date.len() > 0 {
+            xml.push_str(&format!("&lt;upnp:date&gt;{}&lt;/upnp:date&gt;", self.date));
+        }
+
+        for language in &self.languages {
+            xml.push_str(&format!(
+                "&lt;dc_language&gt;{}&lt;/dc:language&gt;",
+                language
+            ));
+        }
+
+        for artist in &self.artists {
+            xml.push_str(&format!(
+                "&lt;upnp:artist&gt;{}&lt;/upnp:artist&gt;",
+                artist
+            ));
+        }
+
+        for copyright in &self.copyrights {
+            xml.push_str(&format!(
+                "&lt;dc:rights&gt;{}&lt;/dc:rightse&gt;",
+                copyright
+            ));
+        }
 
         xml
     }
@@ -241,6 +356,11 @@ impl Item {
     /// Insert new Meta Data.
     /// This is used to parse back the Databases Entries, but not for
     /// reading the Media File itself.
+    ///
+    /// # Arguments
+    ///
+    /// * `name` - Name of the Meta Data
+    /// * `value` - Value of the Meta Data
     pub fn insert_meta_data(&mut self, name: &str, value: &str) {
         match name {
             "title" => self.meta_data.title = value.to_string(),
@@ -284,23 +404,30 @@ impl Item {
         pair_vec
     }
 
+    /// Will generate the UPnP XML String representing this Media Item containing
+    /// all available Values.
+    ///
+    /// # Arguments
+    ///
+    /// * `renderer_cfg` - Configuration for the Renderer that should be served
+    /// * `server_cfg` - The Servers Configuration
     pub fn generate_upnp_xml(
         &self,
         renderer_cfg: &RendererConfiguration,
         server_cfg: &ServerConfiguration,
     ) -> String {
-        let mut title: String = String::new();
-        let mut xml: String = String::from("&lt;item id=\"");
-        xml.push_str(&self.id.to_string());
-        xml.push_str("\" parentID=\"");
-        xml.push_str(&self.parent_id.to_string());
-        xml.push_str("\" restricted=\"1\"&gt;");
+        // Generat first set of Parameters
+        let mut title: String = self.meta_data.file_name.clone();
+        let mut xml: String = format!(
+            "&lt;item id=\"{}\" parentID=\"{}\" restricted=\"1\"&gt;",
+            self.id,
+            self.parent_id
+        );
 
+        // Add title how it is configured
         if renderer_cfg.title_instead_of_name {
             title = self.meta_data.title.clone();
         } else {
-            title = self.meta_data.file_name.clone();
-
             if renderer_cfg.hide_file_extension == false {
                 title.push_str(".");
                 title.push_str(&self.meta_data.file_extension);
@@ -308,42 +435,26 @@ impl Item {
 
         }
 
+        // Replace some special chars to avoid some renderer problems
         title = title.replace("&amp;", " u. ").replace("&", " u. ");
 
-        xml.push_str("&lt;dc:title&gt;");
-        xml.push_str(&title);
-        xml.push_str("&lt;/dc:title&gt;");
+        // Add title,res String and Mime Type
+        xml.push_str(&format!("&lt;dc:title&gt;{}&lt;/dc:title&gt;&lt;res xmlns:dlna=\"urn:schemas-dlna-org:metadata-1-0/\" protocolInfo=\"http-get:*:{}:DLNA.ORG_OP=11;DLNA.ORG_CI=0\" ", title, self.get_mime_type()));
 
-        xml.push_str(
-            "&lt;res xmlns:dlna=\"urn:schemas-dlna-org:metadata-1-0/\" protocolInfo=\"http-get:*:",
-        );
-        xml.push_str(&self.get_mime_type());
-        xml.push_str(":DLNA.ORG_OP=11;DLNA.ORG_CI=0\" ");
-
+        // Add MediaType specifig Values
         match self.media_type {
             MediaType::PICTURE => {
-                xml.push_str("bitrate=\"");
-                xml.push_str(&self.get_bitrate().to_string());
-                xml.push_str("\" ");
-
-
-                xml.push_str("duration=\"");
-                xml.push_str(&self.duration);
-                xml.push_str("\" ");
-
-                xml.push_str("nrAudioChannels=\"");
-                xml.push_str(&self.get_audio_channels().to_string());
-                xml.push_str("\" ");
-
-                xml.push_str("sampleFrequency=\"");
-                xml.push_str(&self.get_sample_rate().to_string());
-                xml.push_str("\" ");
+                xml.push_str(&format!(
+                    "bitrate=\"{}\" duration=\"{}\" nrAudioChannels=\"{}\" sampleFrequency=\"{}\" ",
+                    self.get_bitrate(),
+                    self.duration,
+                    self.get_audio_channels(),
+                    self.get_sample_rate()
+                ));
 
                 match self.media_type {
                     MediaType::VIDEO => {
-                        xml.push_str("resolution=\"");
-                        xml.push_str(&self.get_resolution());
-                        xml.push_str("\" ");
+                        xml.push_str(&format!("resolution=\"{}\" ", self.get_resolution()));
                     }
                     _ => {}
                 }
@@ -351,17 +462,17 @@ impl Item {
             _ => {}
         }
 
-        xml.push_str("size=\"");
-        xml.push_str(&self.file_size.to_string());
-        xml.push_str("\"&gt;");
-        xml.push_str("http://");
-        xml.push_str(&server_cfg.server_ip);
-        xml.push_str(":");
-        xml.push_str(&server_cfg.server_port.to_string());
-        xml.push_str("/stream/");
-        xml.push_str(&self.id.to_string());
-        xml.push_str("&lt;/res&gt;");
+        // Add Values always required
+        xml.push_str(
+            &format!("size=\"{}\"&gt;http://{}:{}/stream/{}&lt;/res&gt;",
+            self.file_size,
+            server_cfg.server_ip,
+            server_cfg.server_port,
+            self.id,
+            ),
+        );
 
+        // Add UPnP Media Type
         match self.media_type {
             MediaType::UNKNOWN => {
                 xml.push_str("&lt;upnp:class&gt;object.item.imageItem&lt;/upnp:class&gt;")
@@ -377,13 +488,16 @@ impl Item {
             }
         }
 
-        xml.push_str(&self.meta_data.generate_upnp_xml());
-
-        xml.push_str("&lt;/item&gt;");
+        // Add additional Meta Data and close Tag
+        xml.push_str(&format!(
+            "{}&lt;/item&gt;",
+            self.meta_data.generate_upnp_xml()
+        ));
 
         xml
     }
 
+    /// Calculates the total Bitrate this Item provides summing each Streams individual Bitrate
     fn get_bitrate(&self) -> u64 {
         let mut rate: u64 = 0;
 
@@ -394,9 +508,13 @@ impl Item {
         rate
     }
 
+    /// Returns the Number of Audio Channels of the default Audio Stream
+    ///
+    /// # TO-DO
+    /// - Match with the requested Stream instead of default one
     fn get_audio_channels(&self) -> u8 {
         for stream in &self.media_tracks {
-            if stream.audio_channels != 0 {
+            if stream.audio_channels != 0 && stream.is_default {
                 return stream.audio_channels;
             }
         }
@@ -404,9 +522,13 @@ impl Item {
         0
     }
 
+    /// Returns the Sample Rate in Hz of the default Audio Stream
+    ///
+    /// # TO-DO
+    /// - Match with the requested Stream instead of default one
     fn get_sample_rate(&self) -> u32 {
         for stream in &self.media_tracks {
-            if stream.sample_rate != 0 {
+            if stream.sample_rate != 0 && stream.is_default {
                 return stream.sample_rate;
             }
         }
@@ -414,21 +536,24 @@ impl Item {
         0
     }
 
+    /// Returns the Resulution as WidthxHeight String for the default Video Stream
+    ///
+    /// # TO-DO
+    /// - Match with the requested Stream instead default one
     fn get_resolution(&self) -> String {
         for stream in &self.media_tracks {
-            if stream.frame_width != 0 && stream.frame_height != 0 {
-                let mut resolution: String = String::new();
-                resolution.push_str(&stream.frame_width.to_string());
-                resolution.push_str("x");
-                resolution.push_str(&stream.frame_height.to_string());
-
-                return resolution;
+            if stream.frame_width != 0 && stream.frame_height != 0 && stream.is_default {
+                return format!("{}x{}", stream.frame_width, stream.frame_height.to_string());
             }
         }
 
         String::new()
     }
 
+    /// Returns the Mime Type used for this Item
+    ///
+    /// # TO-DO
+    /// Use the Format Containers to determine Mime Type instead of static lists
     pub fn get_mime_type(&self) -> String {
         match self.media_type {
             MediaType::VIDEO => {
